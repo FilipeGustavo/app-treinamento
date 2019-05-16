919d849bcb283368073e6f5adc8013896912fa0e
Samira Costa <samiramanuelac@gmail.com>
2019-05-16 14:45:14 -0300

    create routes module customers

--- src/app/pages/customers/customers.module.ts
+++ src/app/pages/customers/customers.module.ts
@@ -2,11 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomersCreateEditComponent } from './customers-create-edit/customers-create-edit.component';
 import { CustomersListComponent } from './customers-list/customers-list.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: "customers/create", component: CustomersCreateEditComponent },
+  { path: "customers/edit", component: CustomersCreateEditComponent },
+  { path: "customers", component: CustomersListComponent }
+];
 
 @NgModule({
   declarations: [CustomersCreateEditComponent, CustomersListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
+  
 })
 export class CustomersModule { }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PagesComponent } from './pages.component';
+
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: "", loadChildren: "src/app/pages/product/product.module#ProductModule" }
+  { path: "", loadChildren: "src/app/pages/product/product.module#ProductModule" },
+  { path: "", loadChildren: "src/app/pages/customers/customers.module#CustomersModule" }
 ];
 
 @NgModule({
