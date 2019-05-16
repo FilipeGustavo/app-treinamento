0a426bce24a8eca4dd5c541d70db2086e031d6a2
Samira Costa <samiramanuelac@gmail.com>
2019-05-16 13:00:11 -0300

    create customers module and component

---
+++ src/app/pages/customers/customers-create-edit/customers-create-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-create-edit',
+  templateUrl: './customers-create-edit.component.html',
+  styleUrls: ['./customers-create-edit.component.scss']
+})
+export class CustomersCreateEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
