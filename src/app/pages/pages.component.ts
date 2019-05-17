a8069db6d44eeb3195903cf677d8a9eafc51dca6
Filipe Gustavo <filipegustavofglv@gmail.com>
2019-05-17 09:10:14 -0300

    Inclusão dos component product e alterações no service

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GenericService } from '@shared/service/generic.service';
+import { Product } from '@shared/domain/product.model';
+import { BaseModel } from '@shared/domain/base.model';
+import { Category } from '@shared/domain/category';
 
 @Component({
   selector: 'app-pages',
